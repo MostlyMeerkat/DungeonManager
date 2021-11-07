5bb546fc2143805aa6f02d60d9f6e0a575e3334f
TJ Maxwell <tjmaxwell157@gmail.com>
2021-11-06 20:01:50 -0600

    in progress

--- Vue/dungeon-manager/src/main.js
+++ Vue/dungeon-manager/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import mock from './mock-weather.js'
 
 Vue.config.productionTip = false
+
+let data = {
+	weather: mock
+}
 
 new Vue({
   router,
---
+++ Vue/dungeon-manager/src/mock-weather.js
@@ -0,0 +1,31 @@
+let mock = [{
+		id: 1,
+		title: "Sunny",
+		image: 'sunny.jpg'
+	},
+	{
+		id: 2,
+		title: "Partly Cloudy",
+		image: 'partly-cloudy.png'
+	},
+	{
+		id: 3,
+		title: "rainy",
+		image: 'rainy.____'
+	},
+	{
+		id: 4,
+		title: "Snowy",
+		image: 'snowy._____'
+	},
+	{
+		id: 5,
+		title: "Thunderstorms",
+		image: 'thunderstorms._____'
+	},
+	{
+		id: 6,
+		title: "Fog",
+		image: 'fog._____'
+	}
+]
