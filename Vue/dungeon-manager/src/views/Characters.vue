61168c2c2a4c5fdd454303062a64b61fab6072c6
Olivia Wynn <oliviaw.randomnumber@gmail.com>
2021-12-04 13:55:04 -0700

    added dropdown html

--- Vue/dungeon-manager/src/views/Characters.vue
+++ Vue/dungeon-manager/src/views/Characters.vue
@@ -1,7 +1,15 @@
 <template>
-  <div id="characters">
+  <div>
     <h1 class='darkText'>Characters</h1>
-    <div class="charsheet-spread">
+    <div class="dropdown">
+      <form>
+        <select name="characters" id="characters">
+          <option v-for="character in characters" :value="character.charName" :key="character.id">
+          </option>
+        </select>
+      </form>
+    </div>
+    <div class="charsheet">
       <Charsheet></Charsheet>
     </div>
   </div>
@@ -9,10 +17,23 @@
 
 <script>
 import Charsheet from '../components/Charsheet.vue';
+import axios from '../axios';
 export default {
   name: 'Characters',
+  methods: {
+    async getCharacters() { //get characters from api
+        try {
+          let response = await axios.get("/api/characters");
+          this.characters = response.data;
+          return true;
+        }
+        catch (error) {
+          console.log(error);
+        }
+    },
+  },
   components: {
     Charsheet
-  }
+  },
 }
 </script>
